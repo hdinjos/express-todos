c95b0963c26cb3dae73829eae1af475e9d69a701
hdinjos <hdinjos@gmail.com>
2021-05-19 22:34:42 +0700

    add production

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import express from 'express';
 import path from 'path';
 import { renderFile } from 'eta';
 import { todos, items } from './routes';
-
 const app = express();
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //configure tamplate engine
 app.engine("eta", renderFile);
 app.set("view engine", "eta");
-app.set("views", "./views");
+app.set("views", "./src/views");
 
 app.use(express.urlencoded({
   extended: true
