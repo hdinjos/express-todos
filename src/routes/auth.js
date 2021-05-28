f01bf3af38081eaf75da844bfc54e50c6aab3d14
hdinjos <hdinjos@gmail.com>
2021-05-29 02:33:36 +0700

    alert register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { loginView, loginPost } from "../controllers/auth/login";
 import logout from "../controllers/auth/logout";
+import { registerView, registerAction } from "../controllers/auth/register";
 const auth = express.Router();
 
 auth.route("/login").get(loginView).post(loginPost);
 auth.get("/logout", logout);
+auth.route("/register").get(registerView).post(registerAction);
 
 export default auth;
