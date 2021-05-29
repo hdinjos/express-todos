da77ea320406c9eda32984c17a8deeaef83cb422
hdinjos <hdinjos@gmail.com>
2021-05-30 02:50:12 +0700

    add bcryptjs and logic register

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,22 +1,36 @@
+import { genSaltSync, hashSync } from "bcryptjs";
+import pool from "../../controllers/config";
+
 const registerView = (req, res) => {
   res.render("auth/register");
 };
 
-const registerAction = (req, res) => {
-  let password = req.body.password;
-  let retypePassword = req.body.retypePassword;
+const registerAction = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const retypePassword = req.body.retypePassword;
   if (password !== retypePassword) {
     res.render("auth/register", {
       err: 0,
       msg: "Password and retype password must same",
     });
   } else {
-    req.session.email = req.body.email;
+    req.session.email = email;
     if (!req.session.email) {
-      // return;
       res.redirect("/auth/register");
     } else {
-      res.redirect("/");
+      try {
+        const salt = genSaltSync(10);
+        const hash = hashSync(password, salt);
+        const query = {
+          text: `INSERT INTO auth(email, password) VALUES($1,$2)`,
+          values: [email, hash],
+        };
+        await pool.query(query);
+        res.redirect("/");
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
 };
