09afe8bf7b6ceb476d264f10c11e08877b6392e8
hdinjos <hdinjos@gmail.com>
2021-06-01 00:36:47 +0700

    update profile

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,12 +1,40 @@
 import pool from "../config";
 
-const detailProfile = (req, res) => {
-  console.log(req.session.email, "jsjsdj");
+const detailProfile = async (req, res) => {
+  const query = {
+    text: `SELECT name, address FROM users INNER JOIN auth ON users.auth_id=auth.id WHERE email=$1`,
+    values: [req.session.email],
+  };
+  const users = await pool.query(query);
   res.render("user/index", {
     name: "Hendi",
     user: req.session.email,
     role: req.session.role,
+    names: users.rows[0].name,
+    address: users.rows[0].address,
   });
 };
 
-export { detailProfile };
+const editProfile = async (req, res, next) => {
+  try {
+    const email = req.session.email;
+    const name = req.body.names;
+    const address = req.body.address;
+    const queryGetAuth = {
+      text: `SELECT id FROM auth WHERE email=$1`,
+      values: [email],
+    };
+    const idAuth = await pool.query(queryGetAuth);
+    const auth_id = idAuth.rows[0].id;
+    const queryUpdateUser = {
+      text: `UPDATE users SET name=$1, address=$2 WHERE auth_id=$3`,
+      values: [name, address, auth_id],
+    };
+    await pool.query(queryUpdateUser);
+    res.redirect("/user/profile");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { detailProfile, editProfile };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
-import { detailProfile } from "../controllers/user";
+import { detailProfile, editProfile } from "../controllers/user";
 import expres from "express";
 
 const user = expres.Router();
 user.get("/profile", detailProfile);
+user.post("/profile/update", editProfile);
 
 export default user;
