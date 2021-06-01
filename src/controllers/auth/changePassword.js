6cb580d66546af7ec42b9d2959f10127b3ac3c73
hdinjos <hdinjos@gmail.com>
2021-06-01 12:25:34 +0700

    change password oke, alert not complete

--- src/controllers/auth/changePassword.js
+++ src/controllers/auth/changePassword.js
@@ -1,12 +1,10 @@
 import pool from "../config";
-import { compareSync } from "bcryptjs";
+import { compareSync, hashSync, genSaltSync } from "bcryptjs";
 
 const changePassword = async (req, res) => {
-  const { email } = req.session;
-  const { oldPassword } = req.body;
-  const { newPassword } = req.body;
-  const { newRetypePassword } = req.body;
   try {
+    const { oldPassword, newPassword, retypeNewPassword } = req.body;
+    const { email } = req.session;
     const queryCurrentPass = {
       text: `SELECT password FROM auth WHERE email=$1`,
       values: [email],
@@ -17,10 +15,29 @@ const changePassword = async (req, res) => {
       currentPassword.rows[0].password
     );
     if (comparePass) {
-      console.log("oke bosq");
-      // const queryChangePassword
+      console.log(newPassword, retypeNewPassword);
+      if (newPassword === retypeNewPassword) {
+        const saltPass = genSaltSync(10);
+        const hashPass = hashSync(newPassword, saltPass);
+        const queryChangePassword = {
+          text: `UPDATE auth SET password=$1 WHERE email=$2`,
+          values: [hashPass, email],
+        };
+        await pool.query(queryChangePassword);
+        req.session.destroy((err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect("/auth/login");
+          }
+        });
+      } else {
+        res.redirect(
+          "/user/profile/?err=1&msg=new+password+and+retype+new+password+must+same"
+        );
+      }
     } else {
-      console.log("wowoow");
+      res.redirect("/user/profile/?err=0&msg=old+password+is+not+valid");
     }
   } catch (err) {
     console.log(err);
