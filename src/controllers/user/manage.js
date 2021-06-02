199d50b3f57972364ed26baf1e6b2508f4017e50
hdinjos <hdinjos@gmail.com>
2021-06-03 02:54:44 +0700

    change role users

--- src/controllers/user/manage.js
+++ src/controllers/user/manage.js
@@ -1,6 +1,17 @@
-const manage = (req, res) => {
+import pool from "../config";
+
+const manageView = async (req, res) => {
   const { email, role } = req.session;
-  res.render("user/manage", { name: "Hendi", role: role, user: email });
+  const query = {
+    text: `SELECT email, role FROM auth ORDER BY id`,
+  };
+  const { rows } = await pool.query(query);
+  res.render("user/manage", {
+    name: "Hendi",
+    role: role,
+    user: email,
+    listUsers: rows,
+  });
 };
 
-export default manage;
+export default manageView;
