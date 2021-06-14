52e54241f190b48c0273e2d5716495e7a27ccd43
hdinjos <hdinjos@gmail.com>
2021-06-15 02:06:50 +0700

    filtering list user in management user

--- src/controllers/user/manage.js
+++ src/controllers/user/manage.js
@@ -19,11 +19,16 @@ const manageRoleAct = async (req, res) => {
     const role = req.body.role;
     const userId = parseInt(req.body.userId);
     const queryRole = {
-      text: `UPDATE auth SET role=$1 WHERE id=$2`,
+      text: `UPDATE auth SET role=$1 WHERE id=$2 RETURNING *`,
       values: [role, userId],
     };
-    await pool.query(queryRole);
-    res.redirect("/user/manage");
+    const resUpdateRole = await pool.query(queryRole);
+    if (req.session.email === resUpdateRole.rows[0].email) {
+      req.session.role = resUpdateRole.rows[0].role;
+      res.redirect("/");
+    } else {
+      res.redirect("/user/manage");
+    }
   } catch (err) {
     console.log(err);
   }
